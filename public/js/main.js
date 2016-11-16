e025f5aa36a701ebcdb8c6af5e27fc140de39e6f
tanvirt <tanvirt@ufl.edu>
2016-11-15 22:24:43 -0600

    Added angular. Moved device test.

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,11 @@
-window.ondevicemotion = function(event) {
-	var ax = event.accelerationIncludingGravity.x
-	var ay = event.accelerationIncludingGravity.y
-	var az = event.accelerationIncludingGravity.z
-	var rotation = event.rotationRate;
+angular.module("main", []);
 
-	document.getElementById("ax").innerHTML = "ax: " + ax;
-	document.getElementById("ay").innerHTML = "ay: " + ay;
-	document.getElementById("az").innerHTML = "az: " + az;
+angular.module("main").run(function($rootScope) {
 
-	if(rotation != null) {
-		var arAlpha = Math.round(rotation.alpha);
-		var arBeta = Math.round(rotation.beta);
-		var arGamma = Math.round(rotation.gamma);
+	Theme.init();
 
-		document.getElementById("arAlpha").innerHTML = "arAlpha: " + arAlpha;
-		document.getElementById("arBeta").innerHTML = "arBeta: " + arBeta;
-		document.getElementById("arGamma").innerHTML = "arGamma: " + arGamma;
-	}
-}
+	$rootScope.flipLogo = function() {
+    	document.getElementById("logo").classList.toggle('flip');
+    }
 
-window.ondeviceorientation = function(event) {
-	var alpha = Math.round(event.alpha);
-	var beta = Math.round(event.beta);
-	var gamma = Math.round(event.gamma);
-
-	document.getElementById("alpha").innerHTML = "alpha: " + alpha;
-	document.getElementById("beta").innerHTML = "beta: " + beta;
-	document.getElementById("gamma").innerHTML = "gamma: " + gamma;
-}
+});
