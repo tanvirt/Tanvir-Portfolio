dd35dac78529896f45eaa454622d1e2458f4bf22
tanvirt <tanvirt@ufl.edu>
2016-11-18 06:25:10 -0600

    Create 3D square graphic

--- public/js/graphic.js
+++ public/js/graphic.js
@@ -6,32 +6,30 @@ function my_application() {
 	canvas.onSetup = function() {
 		var object_maker = new GLObjectMaker(canvas);
 		object_maker.identity();
-	    object_maker.box(1, 1, 1);
-	    object_maker.translate([0.3, 0.3, 0]);
-	    object_maker.sphere(12, 1, 1, 1);
-	    object_maker.cylinderX(8, 1.5, 0.2, 0.2);
+	    object_maker.translate([0, 0, 1.5]);
+	    object_maker.box(0.25, 0.25, 0.25);
 	    cube = object_maker.flush();
 
 	    var mat = new GLMaterial(canvas);
-	    mat.setAmbientColor([0.2, 0.2, 0.2]);
-	    mat.setDiffuseColor([1, 1, 1]);
-	    mat.setSpecularColor([0.8, 0.8, 0.8]);
+	    mat.setAmbientColor([0.2,0.2,0.2]);
+	    mat.setDiffuseColor([1,1,1]);
+	    mat.setSpecularColor([0.8,0.8,0.8]);
 	    mat.setSpecularExponent(2);
 	    cube.setMaterial(mat);
-	    cube.setTexture("http://www.visineat.com/js/img/textures/wood_tile2.jpg");
+	    cube.setTexture("js/img/plainWhiteSquare.png");
 
-		cube.getShader().setLightingDirection([-0.5, 1, 1.5]);
-		canvas.setBackgroundColor(0, 0, 0, 0);
+		canvas.setBackgroundColor(0, 0, 0, 1);
 		canvas.setLoadingStatus(false);
 		canvas.onDrag = function(event) { 
-			canvas.getCamera().oneFingerRotate(event, { radius : 2, type : 'polar' }); 
+			canvas.getCamera().oneFingerRotate(event, { radius : 0.5, type : 'polar' }); 
 		};
+		canvas.useRedCyanProjector();
 	};
 
 	canvas.onDraw = function() {
 		var gl = canvas.getGL();
 		var cam = canvas.getCamera();
-		cam.translate([0, 0, -10]);
+		cam.translate([0, 0, -2]);
 
 		cube.updateShader();
 		cube.draw();
