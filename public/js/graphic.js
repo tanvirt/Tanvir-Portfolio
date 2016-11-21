8d7f215feb8f0bc2ab8e26a89af329685ff722f7
tanvirt <tanvirt@ufl.edu>
2016-11-20 20:46:27 -0600

    Started 3D model of logo

--- public/js/graphic.js
+++ public/js/graphic.js
@@ -13,9 +13,10 @@ var body_onload = function() {
 
 	me.pos_x = 0;
 	me.pos_y = 0;
-	me.pos_z = 0;
+	me.pos_z = -2;
 
 	me.orientation = 0;
+	//me.orientation = Math.PI/4;
 
 	me.color_r = 1;
 	me.color_g = 1;
@@ -25,12 +26,140 @@ var body_onload = function() {
 	var canvas = new GLCanvas("graphic-canvas");
 	var logo = new GLObject(canvas);
 
+	var appendLetter = function(object_maker, size, thickness, translation, rotation) {
+		object_maker.translate(translation);
+		object_maker.rotateX(rotation[0]);
+		object_maker.rotateY(rotation[1]);
+	    object_maker.rotateZ(rotation[2]);
+
+	    var delta = size/2 - thickness/2;
+
+	    // horizontal line
+	    object_maker.translate([0, delta, 0]);
+	    object_maker.box(size*0.75, thickness, thickness);
+	    object_maker.translate([0, -delta, 0]);
+
+	    // vertical line
+	    object_maker.box(thickness, size, thickness);
+
+	    object_maker.rotateX(-rotation[0]);
+		object_maker.rotateY(-rotation[1]);
+	    object_maker.rotateZ(-rotation[2]);
+	    object_maker.translate([
+	    	-translation[0],
+	    	-translation[1],
+	    	-translation[2]
+	    ]);
+	}
+
+	var appendSquare = function(object_maker, size, thickness, translation, rotation) {
+		object_maker.translate(translation);
+		object_maker.rotateX(rotation[0]);
+		object_maker.rotateY(rotation[1]);
+	    object_maker.rotateZ(rotation[2]);
+
+	    var delta = size/2 - thickness/2;
+
+	    // top line
+	    object_maker.translate([0, delta, 0]);
+	    object_maker.box(size, thickness, thickness);
+	    object_maker.translate([0, -delta, 0]);
+
+	    /*// bottom line
+	    object_maker.translate([0, -delta, 0]);
+	    object_maker.box(size, thickness, thickness);
+	    object_maker.translate([0, delta, 0]);*/
+
+	    // right line
+	    object_maker.translate([delta, 0, 0]);
+	    object_maker.box(thickness, size, thickness);
+	    object_maker.translate([-delta, 0, 0]);
+
+	    // left line
+	    object_maker.translate([-delta, 0, 0]);
+	    object_maker.box(thickness, size, thickness);
+	    object_maker.translate([delta, 0, 0]);
+
+	    object_maker.rotateX(-rotation[0]);
+		object_maker.rotateY(-rotation[1]);
+	    object_maker.rotateZ(-rotation[2]);
+	    object_maker.translate([
+	    	-translation[0],
+	    	-translation[1],
+	    	-translation[2]
+	    ]);
+	}
+
+	var appendLine = function(object_maker, size, thickness, translation, rotation) {
+		object_maker.translate(translation);
+		object_maker.rotateX(rotation[0]);
+		object_maker.rotateY(rotation[1]);
+	    object_maker.rotateZ(rotation[2]);
+
+	    var delta = size/2 - thickness/2;
+
+	    // horizontal line
+	    object_maker.box(size, thickness, thickness);
+
+	    object_maker.rotateX(-rotation[0]);
+		object_maker.rotateY(-rotation[1]);
+	    object_maker.rotateZ(-rotation[2]);
+	    object_maker.translate([
+	    	-translation[0],
+	    	-translation[1],
+	    	-translation[2]
+	    ]);
+	}
+
 	canvas.onSetup = function() {
 		var object_maker = new GLObjectMaker(canvas);
 		object_maker.identity();
-		object_maker.translate([0, 0, 0]);
-	    object_maker.box(0.25, 0.25, 0.25);
-	    //object_maker.box(0.1, 0.1, 0.1);
+
+		// top letter
+		appendLetter(
+	    	object_maker,
+	    	0.5,
+	    	0.05,
+	    	[-0.5, 0.5, 0],
+	    	[0, 0, 0]
+	    );
+
+	    // top square
+	    appendSquare(
+	    	object_maker,
+	    	1,
+	    	0.05,
+	    	[-0.5, 0.5, 0],
+	    	[0, 0, Math.PI/4]
+	    );
+
+	    // center Line
+	    appendLine(
+	    	object_maker,
+	    	2.125,
+	    	0.05,
+	    	[0, 0, 0],
+	    	[0, 0, Math.PI/4]
+	    );
+
+	    // bottom letter
+	    appendLetter(
+	    	object_maker,
+	    	0.5,
+	    	0.05,
+	    	[0.5, -0.5, 0],
+	    	[0, 0, 0]
+	    );
+
+	    // bottom square
+	    appendSquare(
+	    	object_maker,
+	    	1,
+	    	0.05,
+	    	[0.5, -0.5, 0],
+	    	[0, 0, -3*Math.PI/4]
+	    );
+
 	    logo = object_maker.flush();
 
 	    var material = new GLMaterial(canvas);
@@ -41,7 +170,6 @@ var body_onload = function() {
 	    logo.setMaterial(material);
 	    logo.setTexture("js/img/white_square.png");
 
-	    //logo.setDrawModeLines();
 	    logo.onTap = function(event) {
 	    	if(graphicSelected) {
 	    		graphicSelected = false;
@@ -52,6 +180,7 @@ var body_onload = function() {
 	    		graphicSelected = true;
 	    		canvas.setBackgroundColor(0, 0, 0, 0.75);
 	    		canvas.useRedCyanProjector();
+	    		//logo.setDrawModeLines();
 	    	}
 	    }
 
